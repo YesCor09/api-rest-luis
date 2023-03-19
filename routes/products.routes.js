f2934d51c24be583a0a6762922aa4f91fbd7e688
YesCor09 <65466945+YesCor09@users.noreply.github.com>
2023-03-18 23:09:30 -0600

    Add files via upload

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express'
-import {getProducts, createProduct, updateProduct, deleteProduct, getProduct} from '../controllers/products.controller.js'
+import {getProducts, createProduct, updateProduct, deleteProduct, getProduct, getProductCategorie} from '../controllers/products.controller.js'
 import fileUpload from 'express-fileupload'
 
 const router = Router()
 
 router.get('/products', getProducts)
 router.get('/products/:id', getProduct)
+router.post('getProductsCategorie', getProductCategorie)
 router.post('/products', fileUpload({useTempFiles : true, tempFileDir : './uploads'}), createProduct)
 router.put('/products/:id', updateProduct)
 router.delete('/products/:id', deleteProduct)
