02b72ff1081e15e2e308aad92aa1d877dfa67784
YesCor09 <65466945+YesCor09@users.noreply.github.com>
2023-03-22 22:36:24 -0600

    Add files via upload

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -16,10 +16,10 @@ export const createProduct = async (req, res) => {
         const {name,description,price,categorie} = req.body
         
         const product = new Product({
-        name,
-        description,
-        price,
-        categorie
+            name,
+            description,
+            price,
+            categorie
         })
 
         if(req.files?.image){
@@ -43,7 +43,7 @@ export const getProduct = async (req, res) => {
         const product = await Product.findById(req.params.id)
 
         if(!product) return res.status(404).json({
-        message: 'El producto no existe'
+            message: 'El producto no existe'
         })
         return res.send(product)
     } catch (error){
@@ -65,11 +65,11 @@ export const deleteProduct = async (req, res) => {
     try {
         const product = await Product.findByIdAndDelete(req.params.id)
         if(!product) return res.status(404).json({
-        message: 'El producto no existe'
+            message: 'El producto no existe'
         })
 
         if(product.image?.public_id){
-        await deleteImage(product.image.public_id)
+            await deleteImage(product.image.public_id)
         }
         
         return res.send(product)
@@ -81,20 +81,32 @@ export const deleteProduct = async (req, res) => {
 export const updateProduct = async (req, res) => {
     try {
         const {id} = req.params;
+
+        const {name,description,price,categorie} = req.body
         
+        const product = ({
+            name,
+            description,
+            price,
+            categorie
+        })
+        
+        const productImage = await Product.findById(id)
+        await deleteImage(productImage.image.public_id)
+
         if(req.files?.image){
             const rs = await uploadImage(req.files.image.tempFilePath)
-            productUpdate.image = {
+            product.image = {
                 public_id: rs.public_id,
                 secure_url: rs.secure_url
             }
-            await fs.unlink(req.files.image.tempFilePath)
+            //await fs.unlink(req.files.image.tempFilePath)
         }
-        
-        const productUpdate = await Product.findByIdAndUpdate(id, req.body, {
+
+        const productUpdate = await Product.findByIdAndUpdate(id, product, {
             new:true
         })
-        
+
         return res.json(productUpdate)
     } catch (error){
         return res.status(500).json({message:error.message})
