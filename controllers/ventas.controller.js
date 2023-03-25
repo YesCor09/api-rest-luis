f2824e497358355064e9c67a6c41f3b38d17a2d4
YesCor09 <65466945+YesCor09@users.noreply.github.com>
2023-03-24 22:34:34 -0600

    Add files via upload

---
+++ controllers/ventas.controller.js
@@ -0,0 +1,45 @@
+import Ventas from '../models/ventas.model.js'
+export const getVentas = async (req, res) => {
+    try{
+        const ventas = await Ventas.find().populate('user').populate('products.product');
+        res.json(ventas)
+    } catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+export const createVenta = async (req, res) => {
+    try {
+        const venta = new Ventas(req.body)
+        await venta.save()
+        res.json(venta)
+    } catch (error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+export const getVenta = async (req, res) => {
+    try {
+        const venta = await Ventas.findById(req.params.id)
+
+        if(!venta) return res.status(404).json({
+            message: 'La venta no existe'
+        })
+        return res.send(venta)
+    } catch (error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+export const deleteVenta = async (req, res) => {
+    try {
+        const venta = await Ventas.findByIdAndDelete(req.params.id)
+        if(!venta) return res.status(404).json({
+            message: 'La venta no existe'
+        })
+        
+        return res.send(venta)
+    } catch (error){
+        return res.status(500).json({message:error.message})
+    }
+}
