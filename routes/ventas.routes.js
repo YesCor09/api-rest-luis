557342b39b4549e31020a795be0c742b3b2a3b69
YesCor09 <65466945+YesCor09@users.noreply.github.com>
2023-03-27 01:47:38 -0600

    Add files via upload

--- routes/ventas.routes.js
+++ routes/ventas.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import {getVentas, createVenta, getVenta, deleteVenta} from '../controllers/ventas.controller.js'
+import {getVentas, createVenta, getVenta, deleteVenta, getVentaByIdUser} from '../controllers/ventas.controller.js'
 
 const router = Router()
 
 router.get('/ventas', getVentas)
 router.get('/ventas/:id', getVenta)
+router.get('/ventasUser/:id', getVentaByIdUser)
 router.post('/ventas', createVenta)
 router.delete('/ventas/:id', deleteVenta)
 
