8008483c6c2b60339dd740c8c1318c27ea1ffdae
YesCor09 <65466945+YesCor09@users.noreply.github.com>
2023-03-27 01:32:39 -0600

    Add files via upload

--- controllers/ventas.controller.js
+++ controllers/ventas.controller.js
@@ -32,6 +32,19 @@ export const getVenta = async (req, res) => {
     }
 }
 
+export const getVentaByIdUser = async (req, res) => {
+    try {
+        const venta = await Ventas.find({user: req.params.id}).populate('user').populate('products.product')
+
+        if(!venta) return res.status(404).json({
+            message: 'Este usuario no ha realizado ninguna compra'
+        })
+        return res.send(venta)
+    } catch (error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
 export const deleteVenta = async (req, res) => {
     try {
         const venta = await Ventas.findByIdAndDelete(req.params.id)
