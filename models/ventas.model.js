387fc1aeced327b77389da87878478ab419b27e3
YesCor09 <65466945+YesCor09@users.noreply.github.com>
2023-03-27 21:53:02 -0600

    Add files via upload

--- models/ventas.model.js
+++ models/ventas.model.js
@@ -6,20 +6,19 @@ const ventasSchema = mongoose.Schema({
         ref: 'User',
         required: true
     },
-    products: [
-        {
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Product',
-                required: true
-            },
-            quantity: {
-                type: Number,
-                required: true,
-                default: 1
-            }
+    products: {
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
         },
-    ],
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1
+        }
+    },
+
     totalPrice: {
         type: Number,
         required: true
